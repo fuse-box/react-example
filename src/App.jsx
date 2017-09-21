a325c46da502f7c2d343de0bf417dd5c453c35f0
s4kr4 <s4kr4m4@gmail.com>
2017-09-22 00:43:00 +0900

    Remove unnecessary semicolon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Component } from 'react';
 import './App.css';
-import * as logo from './logo.svg';;
+import * as logo from './logo.svg';
 
 class App extends Component {
     render() {
