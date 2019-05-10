d5441810419f25105330ae5229223ee7381bf7da
Ivan Orlov <ivan.orlov@zervant.com>
2019-05-10 13:55:33 +0200

    fixes

--- fuse.ts
+++ fuse.ts
@@ -1,11 +1,14 @@
 import { fusebox } from "fuse-box";
 
 const fuse = fusebox({
-  cache: true,
   target: "browser",
   entry: "src/index.jsx",
   webIndex: {
     template: "src/index.html"
+  },
+  cache: {
+    root: ".cache",
+    enabled: true
   },
   watch: true,
   hmr: true,
