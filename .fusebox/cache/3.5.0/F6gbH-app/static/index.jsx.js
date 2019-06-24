fd69fd342f357480ce3f33a3f5d3ae8928a3eab2
nchanged <nchanged@gmail.com>
2019-06-24 08:36:32 +0200

    version update

---
+++ .fusebox/cache/3.5.0/F6gbH-app/static/index.jsx.js
@@ -0,0 +1,9 @@
+module.exports = { contents: "\"use strict\";\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar moment = require(\"moment\");\nconsole.log(moment);\n",
+dependencies: ["moment"],
+sourceMap: {},
+headerContent: undefined,
+mtime: 1557930375990,
+devLibsRequired : undefined,
+ac : undefined,
+_ : {}
+}
--- fuse.ts
+++ fuse.ts
@@ -1,18 +1,44 @@
-import { fusebox } from "fuse-box";
+import { fusebox, sparky } from "fuse-box";
 
-const fuse = fusebox({
-  target: "browser",
-  entry: "src/index.jsx",
-  webIndex: {
-    template: "src/index.html"
-  },
-  cache: {
-    root: ".cache",
-    enabled: true
-  },
-  watch: true,
-  hmr: true,
-  devServer: true,
-  logging: { level: "succinct" }
+class Context {
+  isProduction;
+  runServer;
+  getConfig() {
+    return fusebox({
+      target: "browser",
+      entry: "src/index.jsx",
+      webIndex: {
+        template: "src/index.html"
+      },
+      cache: {
+        root: ".cache",
+        enabled: true
+      },
+      env: { NODE_ENV: this.isProduction ? "production" : "development" },
+      watch: true,
+      hmr: true,
+      devServer: this.runServer,
+      logging: { level: "succinct" }
+    });
+  }
+}
+const { task, exec } = sparky<Context>(Context);
+
+task("default", async ctx => {
+  ctx.runServer = true;
+  const fuse = ctx.getConfig();
+  await fuse.runDev();
 });
-fuse.runDev();
+
+task("preview", async ctx => {
+  ctx.runServer = true;
+  ctx.isProduction = true;
+  const fuse = ctx.getConfig();
+  await fuse.runProd({uglify : false});
+});
+task("dist", async ctx => {
+  ctx.runServer = false;
+  ctx.isProduction = true;
+  const fuse = ctx.getConfig();
+  await fuse.runProd({ uglify: false });
+});
